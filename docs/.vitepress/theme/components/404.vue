6c3531e303f3785dd6af3217b6f940529d50ce08
TomChicken-on-Github <Tom_Chicken@qq.com>
2026-02-25 17:15:16 +0800

    fix: update 404 error message for clarity

--- docs/.vitepress/theme/components/404.vue
+++ docs/.vitepress/theme/components/404.vue
@@ -11,7 +11,7 @@ const ns = "error-page";
     <img :src="withBase('/404.png')" :class="`${ns}__img`" alt="404" />
     <div :class="[`${ns}__detail`, 'flx-column']">
       <h2>404</h2>
-      <h4>你是不是走错地方了喵！🤷‍♂️🤷‍♀️</h4>
+      <h4>杂鱼是不是走错地方了喵！</h4>
       <button @click="router.go('/')">返回首页</button>
     </div>
   </div>
