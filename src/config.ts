1f1342c38ba767c510ebb00cd96ad0b93702e7de
TomChicken-on-Github <Tom_Chicken@qq.com>
2026-03-08 18:45:20 +0800

    Update top banner

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			name: "Server Status",
+			url: "https://monitor.tomchicken.icu", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
